c55a3649e058f3a9266eb2d746b4f82ba32c4ac4
Issac <issacqzh@gmail.com>
2020-02-25 14:05:57 -0500

    first commit

---
+++ ollii_project/ollii_site/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+	path('',views.home,name='home'),
+	path('about/',views.about,name='about'),
+	path('help4mom/',views.help4mom,name='help4mom'),
+	path('vocabulary/',views.vocabulary,name='vocabulary')
+]
---
+++ ollii_project/ollii_site/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def home(request):
+	return render(request,'html_sites/home.html')
+
+def about(request):
+	return render(request,'html_sites/about.html')
+
+def help4mom(request):
+	return render(request,'html_sites/help4mom.html')
+
+def vocabulary(request):
+	return render(request,'html_sites/vocabulary.html')
