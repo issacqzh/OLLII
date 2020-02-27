3261bb5d4f78ca3434ed10115ed373b2529145b3
Issac <issacqzh@gmail.com>
2020-02-27 09:43:03 -0500

    latest version

--- ollii_project/ollii_project/settings.py
+++ ollii_project/ollii_project/settings.py
@@ -133,6 +133,9 @@ STATIC_URL = '/static/'
 
 MIDDLEWARE = [
   'django.middleware.security.SecurityMiddleware',
+  'django.contrib.sessions.middleware.SessionMiddleware',
+'django.contrib.auth.middleware.AuthenticationMiddleware',
+'django.contrib.messages.middleware.MessageMiddleware',
   'whitenoise.middleware.WhiteNoiseMiddleware',
   # ...
 ]
