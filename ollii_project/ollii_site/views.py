aa3151c7240c448fd8501e0d21265e027f4224f2
Issac <issacqzh@gmail.com>
2020-03-23 20:16:54 -0400

    definition update

--- ollii_project/ollii_site/views.py
+++ ollii_project/ollii_site/views.py
@@ -65,71 +65,80 @@ def implementor(request):
 
 
 def definition(request):
-    return render(request,'html_sites/definition.html',{})
-    # if 'term' in request.GET:
-    #     print('s')
-    # else:
-    #     return render(request,'html_sites/search.html',{})
+    # return render(request,'html_sites/definition.html',{})
+    if 'term' in request.GET:
+        print('s')
+    else:
+        return render(request,'html_sites/definition.html',{})
 
-    # basic_address='https://eutils.ncbi.nlm.nih.gov/entrez/eutils/'
-    # search_address='esearch.fcgi?db=mesh&retmax=100&term='
-    # summary_address='esummary.fcgi?db=mesh&retmax=100&id='
-    # id_results=[]
-    # with requests.Session() as s:
+    basic_address='https://eutils.ncbi.nlm.nih.gov/entrez/eutils/'
+    search_address='esearch.fcgi?db=mesh&retmax=30&term='
+    summary_address='esummary.fcgi?db=mesh&retmax=30&id='
+    id_results=[]
+    with requests.Session() as s:
 
-    #     response = s.get(basic_address+search_address+request.GET.get('term'))
-    #     tree = ElementTree.fromstring(response.content)
-    #     for result in tree.find('IdList'):
-    #         id_results.append(result.text)
-    #     s.close()
+        response = s.get(basic_address+search_address+request.GET.get('term'))
+        tree = ElementTree.fromstring(response.content)
+        id_list=tree.find('IdList')
+        for id_ in id_list:
+            id_results.append(id_.text)
+
     
-    # final_id=None
-    # final_term=None
-    # with requests.Session() as s:
-    #     for i in range(len(id_results)):
-            
-    #         response=s.get(basic_address+summary_address+id_results[i])
-    #         print(len(id_results))
-    #         print(id_results[i])
-    #         print(response.content)
-    #         tree=ElementTree.fromstring(response.content)
-    #         for item in tree[0].findall('Item'):
-    #             if item.attrib['Name']=="DS_MeshTerms":
-    #                 for mesh in item:
-    #                     if mesh.text.lower() == request.GET.get('term').lower():
-    #                         final_id=id_results[i]
-    #                         final_term = mesh.text
-    #                         break
-            
-    #         time.sleep(1)
-    #         if i >2: break
-    #     s.close()
-    # if final_id is None:
-    #     print("None")
-    # else:
-    #     with requests.Session() as s:
-    #         result_response=s.get(basic_address+summary_address+final_id)
-    #         tree = ElementTree.fromstring(result_response.content)
-    #         related=[]
-    #         for item in tree[0].findall('Item'):
-    #             if item.attrib['Name'] == "DS_ScopeNote":
-    #                 definition= item.text
-    #             elif item.attrib['Name']=="DS_SeeRelated":
-    #                 for row in item:
-    #                     related.append(row.text)
-    #             elif item.attrib['Name']=="DS_IdxLinks":
-    #                 for linked_tree in item:
-    #                     for tree_item in linked_tree:
-    #                         if tree_item.attrib['Name'] == "Parent":
-    #                             related.append(tree_item.text)
-    #                         elif tree_item.attrib['Name'] == "Children":
-    #                             for child in tree_item:
-    #                                 related.append(child.text)
-    #                         else:
-    #                             continue    
-    #         context={'term':final_term,'definition':definition, 'related':related}
-    #         s.close()
-    # return render(request,'html_sites/search.html',context)   
+    final_id=None
+    final_term=None
+    final_def=None
+    with requests.Session() as s:
+        for i in range(len(id_results)):
+     
+            response=s.get(basic_address+summary_address+id_results[i])
+        
+            tree=ElementTree.fromstring(response.content)
+            temp_def=None
+            for item in tree[0].findall('Item'):
+                if item.attrib['Name']=="DS_ScopeNote":
+                    temp_def= item.text
+                if item.attrib['Name']=="DS_MeshTerms":
+                    for mesh in item:
+                        if mesh.text.lower() == request.GET.get('term').lower():
+                            final_id=id_results[i]
+                            final_term = mesh.text
+                            final_def=temp_def
+                            break
+            time.sleep(0.4)
+    if final_id is None:
+        context={}
+    else:
+        related=[]
+        related_terms=[]
+        with requests.Session() as s:
+            result_response=s.get(basic_address+summary_address+final_id)
+            tree = ElementTree.fromstring(result_response.content)
+            for item in tree[0].findall('Item'):
+                if item.attrib['Name']=="DS_SeeRelated":
+                    for row in item:
+                        related.append(row.text)
+                elif item.attrib['Name']=="DS_IdxLinks":
+                    for linked_tree in item:
+                        for tree_item in linked_tree:
+                            if tree_item.attrib['Name'] == "Parent":
+                                related.append(tree_item.text)
+                            elif tree_item.attrib['Name'] == "Children":
+                                for child in tree_item:
+                                    related.append(child.text)
+                            else:
+                                continue
+            time.sleep(0.4)   
+            for related_id in related:
+                related_response = s.get(basic_address+summary_address+related_id)
+                related_tree = ElementTree.fromstring(related_response.content) 
+                for item in related_tree[0].findall('Item'):
+                    if item.attrib['Name'] == 'DS_MeshTerms':
+                        related_terms.append(item[0].text)
+                        break
+                time.sleep(0.4)
+        context={'term':final_term,'definition':final_def, 'related':related_terms}
+
+    return render(request,'html_sites/definition.html',context)   
 
         
 
